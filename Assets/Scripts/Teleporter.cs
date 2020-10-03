5ca97fe5f1741ac2cae5029a03669cfd7261f82b
Markus-PC\Markus <markus.brand@student.hpi.de>
2020-10-03 23:14:59 +0200

    Add teleporter

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
+
+public class Teleporter : Rewindable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.HasComponent(out Player player))
+        {
+            Teleport();
+        };
+    }
+
+    private void Teleport()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void loadFrom(object pairValue)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override object save()
+    {
+        throw new System.NotImplementedException();
+    }
+}
