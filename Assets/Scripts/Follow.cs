a0718e8ecdc2616c839882fcf515fcff9ac0e057
Markus Brand <markus.brand@student.hpi.de>
2020-10-04 22:10:47 +0200

    Start follow behaviour

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+	public GameObject followed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
