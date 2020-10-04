f1be508def7bd5ea2628f050287ae9d908c0ef34
Markus-PC\Markus <markus.brand@student.hpi.de>
2020-10-04 11:28:40 +0200

    Start Lever

---
+++ Assets/Lever.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : Rewindable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void loadFrom(object pairValue)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override object save()
+    {
+        throw new System.NotImplementedException();
+    }
+}
